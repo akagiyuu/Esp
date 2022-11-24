1cab3a044c3168fdbaeeff6a4ba60d723fb3c507
AkagiYuu <khanghuynh0245@gmail.com>
2022-11-24 17:57:14 +0700

    Update

--- lib/Health/AbnormalCondition.cpp
+++ lib/Health/AbnormalCondition.cpp
@@ -3,9 +3,9 @@
 uint32 AbnormalCondition::detect(int32_t heart_rate, int32_t spo2)
 {
     uint32 conditions = 0;
-    if(heart_rate < 20 || heart_rate > 100)
+    if(heart_rate < 60 || heart_rate > 100)
         conditions |= UnsualHeartRate;
-    if(spo2 < 20 || spo2 > 100)
+    if(spo2 > 90)
         conditions |= UnsualSPO2;
 
     return conditions;
